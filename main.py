13b0321960c12eaa58a0fa34e6570212113f9acc
yanghetengshan <yanghetengshan@sayinfo.cn>
2019-11-20 18:16:27 +0800

    update simple function to class

--- main.py
+++ main.py
@@ -3,82 +3,124 @@ from pprint import pprint
 
 LENGTH = 9
 WIDTH = 9
-COLOR = 4
+COLOR = 6
 
-def init_puzzle():
-    # init empty puzzle (large 1)
-    puzzle = [[0 for point_x in range(LENGTH)] for point_y in range(WIDTH)]
+class Puzzle(object):
 
-    for point_x in range(LENGTH):
-        for point_y in range(WIDTH):
-            puzzle = fill_block(puzzle, point_x, point_y)
-    
-    return puzzle
-    
-def fill_block(puzzle, point_x, point_y):
-    # ready fill color: set(0, 1, 2, 3)
-    ready_color = set(range(COLOR))
-    # same line duplicate color
-    if point_x-2 >= 0 and puzzle[point_x-1][point_y] == puzzle[point_x-2][point_y]:
-        ready_color.remove(puzzle[point_x-1][point_y])
-    # same column duplicate color
-    if point_y-2 >= 0 and puzzle[point_x][point_y-1] == puzzle[point_x][point_y-2]:
-        if puzzle[point_x][point_y-1] in ready_color:
-            ready_color.remove(puzzle[point_x][point_y-1])
-    # fill one block
-    puzzle[point_x][point_y] = random.choice(list(ready_color))
-    return puzzle
+    def __init__(self, length=LENGTH, width=WIDTH, color=COLOR):
+        self.length = length
+        self.width = width
+        self.color = color
+        self.puzzle = self.__random_init()
 
-def update_puzzle(puzzle, matched):
-    for x, y in matched:
-        for i in range(x, 0, -1):
-            print(i, y, i-1, y)
-            puzzle[i][y], puzzle[i-1][y] = puzzle[i-1][y], puzzle[i][y]
-        puzzle[0][y] = random.choice(range(COLOR))
-    return puzzle
 
-def is_dead(puzzle):
-    for point_x in range(LENGTH):
-        for point_y in range(WIDTH):
-            if point_x-1 >= 0 and puzzle[point_x][point_y] == puzzle[point_x-1][point_y]:
-                if block_status_1(puzzle, point_x, point_y):
-                    return True
-            elif point_y-1 >= 0 and puzzle[point_x][point_y] == puzzle[point_x][point_y-1]:
-                if block_status_2(puzzle, point_x, point_y):
-                    return True
-            elif point_x-2 >= 0 and puzzle[point_x][point_y] == puzzle[point_x-2][point_y]:
-                if block_status_3(puzzle, point_x, point_y):
-                    return True
-            elif point_y-2 >= 0 and puzzle[point_x][point_y] == puzzle[point_x][point_y-2]:
-                if block_status_4(puzzle, point_x, point_y):
-                    return True
-    return False
+    def move(self, point_x, point_y, arrow):
+        matched = self.__match_puzzle(point_x, point_y, arrow)
+        print(matched)
+        while True:
+            changed = self.__update_puzzle(matched)
+            # if self.__is_dead():
+            #     self.puzzle = self.__random_init()
+            #     break
+            matched = list()
+            for x, y in changed:
+                matched += self.__match_blocks(x, y)
+            if len(matched) == 0:
+                break
+            
+
+    def __random_init(self):
+        puzzle = [[0 for point_x in range(self.length)] for point_y in range(self.width)]
+        for point_x in range(self.length):
+            for point_y in range(self.width):
+                puzzle = self.__fill_block(puzzle, point_x, point_y)
+        return puzzle
+
+
+    def __fill_block(self, puzzle, point_x, point_y):
+        ready_color = set(range(self.color))
+        # same line duplicate color
+        if point_x-2 >= 0 and puzzle[point_x-1][point_y] == puzzle[point_x-2][point_y]:
+            ready_color.remove(puzzle[point_x-1][point_y])
+        # same column duplicate color
+        if point_y-2 >= 0 and puzzle[point_x][point_y-1] == puzzle[point_x][point_y-2]:
+            if puzzle[point_x][point_y-1] in ready_color:
+                ready_color.remove(puzzle[point_x][point_y-1])
+        # fill one block
+        puzzle[point_x][point_y] = random.choice(list(ready_color))
+        return puzzle
+
+
+    def __match_puzzle(self, point_x, point_y, arrow):
+        if arrow == 'left':
+            x1, y1, x2, y2 = point_x, point_y, point_x, point_y-1
+        elif arrow == 'right':
+            x1, y1, x2, y2 = point_x, point_y, point_x, point_y+1
+        elif arrow == 'up':
+            x1, y1, x2, y2 = point_x-1, point_y, point_x, point_y
+        elif arrow == 'down':
+            x1, y1, x2, y2 = point_x+1, point_y, point_x, point_y
+        
+        self.puzzle[x1][y1], self.puzzle[x2][y2] = self.puzzle[x2][y2], self.puzzle[x1][y1]
+        matched = self.__match_blocks(x1, y1) + self.__match_blocks(x2, y2)
+        if len(matched) == 0:
+            self.puzzle[x1][y1], self.puzzle[x2][y2] = self.puzzle[x2][y2], self.puzzle[x1][y1]
+        return list(set(matched))
+
+
+    def __match_blocks(self, point_x, point_y):
+        matched = list()
+        for i in range(self.length-2):
+            if self.puzzle[point_x][i] - self.puzzle[point_x][i+1] == self.puzzle[point_x][i+1] - self.puzzle[point_x][i+2] == 0:
+                matched += [(point_x, i), (point_x, i+1), (point_x, i+2)]
+        for i in range(self.width-2):
+            if self.puzzle[i+1][point_y] - self.puzzle[i][point_y] == self.puzzle[i+2][point_y] - self.puzzle[i+1][point_y] == 0:
+                matched += [(i, point_y), (i+1, point_y), (i+2, point_y)]
+        return matched
+
+
+    # todo: can't right update
+    def __update_puzzle(self, matched):
+        changed = list()
+        for x, y in matched:
+            for i in range(x, 0, -1):
+                changed += [(i, y), (i-1, y)]
+                self.puzzle[i][y], self.puzzle[i-1][y] = self.puzzle[i-1][y], self.puzzle[i][y]
+            self.puzzle[0][y] = random.choice(range(self.color))
+            changed.append((0, y))
+        return list(set(changed))
+
+    # miss 4 method
+    def __is_dead(self):
+        for point_x in range(self.length):
+            for point_y in range(self.width):
+                if point_x-1 >= 0 and self.puzzle[point_x][point_y] == self.puzzle[point_x-1][point_y]:
+                    if self.__block_status_1(point_x, point_y):
+                        return True
+                elif point_y-1 >= 0 and self.puzzle[point_x][point_y] == self.puzzle[point_x][point_y-1]:
+                    if self.__block_status_2(point_x, point_y):
+                        return True
+                elif point_x-2 >= 0 and self.puzzle[point_x][point_y] == self.puzzle[point_x-2][point_y]:
+                    if self.__block_status_3(point_x, point_y):
+                        return True
+                elif point_y-2 >= 0 and self.puzzle[point_x][point_y] == self.puzzle[point_x][point_y-2]:
+                    if self.__block_status_4(point_x, point_y):
+                        return True
+        return False
+
+
+    def __block_status_1(self, point_x, point_y):
+        pass
+
+    def __block_status_2(self, point_x, point_y):
+        pass
+
+    def __block_status_3(self, point_x, point_y):
+        pass
+
+    def __block_status_4(self, point_x, point_y):
+        pass
                 
-
-def match_puzzle(puzzle, point_x, point_y, arrow):
-    if arrow == 'left':
-        x1, y1, x2, y2 = point_x, point_y, point_x, point_y-1
-    elif arrow == 'right':
-        x1, y1, x2, y2 = point_x, point_y, point_x, point_y+1
-    elif arrow == 'up':
-        x1, y1, x2, y2 = point_x-1, point_y, point_x, point_y
-    elif arrow == 'down':
-        x1, y1, x2, y2 = point_x+1, point_y, point_x, point_y
-    
-    puzzle[x1][y1], puzzle[x2][y2] = puzzle[x2][y2], puzzle[x1][y1]
-    matched = match_blocks(puzzle, x1, y1) + match_blocks(puzzle, x2, y2)
-
-    return list(set(matched))
-
-def match_blocks(puzzle, point_x, point_y):
-    matched = list()
-    for i in range(LENGTH-2):
-        if puzzle[point_x][i] - puzzle[point_x][i+1] == puzzle[point_x][i+1] - puzzle[point_x][i+2] == 0:
-            matched += [(point_x, i), (point_x, i+1), (point_x, i+2)]
-    for i in range(WIDTH-2):
-        if puzzle[i+1][point_y] - puzzle[i][point_y] == puzzle[i+2][point_y] - puzzle[i+1][point_y] == 0:
-            matched += [(i, point_y), (i+1, point_y), (i+2, point_y)]
-    return list(matched)
 
 def show_image(puzzle):
     import matplotlib.pyplot as plt
@@ -89,19 +131,15 @@ def show_image(puzzle):
     plt.show()
 
 if __name__ == "__main__":
-    puzzle = init_puzzle()
+    p = Puzzle()
+    show_image(p.puzzle)
 
-    # show_image(puzzle)
-    matched = match_puzzle(puzzle, 3, 6, 'up')
-    pprint(matched)
+    x = input()
+    y = input()
+    arrow = input()
+    p.move(int(x), int(y), arrow)
+    show_image(p.puzzle)
 
 
-    for x, y in matched:
-        puzzle[x][y] = 8
-    
-    show_image(puzzle)
-
-    update_puzzle(puzzle, matched)
-
-    show_image(puzzle)
-
+    # for x, y in matched:
+    #     puzzle[x][y] = 8
